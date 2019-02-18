0defac3bdb33c3dff7d609278b13fe7f6387f4c0
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2019-02-18 23:35:15 +0330

    Edited description

--- Schedule Power/Properties/AssemblyInfo.cs
+++ Schedule Power/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Schedule Power")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simply schedule power options")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hirbod Behnam")]
 [assembly: AssemblyProduct("Schedule Power")]
